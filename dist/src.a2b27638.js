30590614a188c13584531703d4995e982337688c
Apoint <136661657+Tenzin928@users.noreply.github.com>
2023-06-16 00:57:18 +0000

    add

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -176,6 +176,9 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+var onClickAdd = function onClickAdd() {
+  alert();
+};
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+    alert();
+}
+
